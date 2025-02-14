2f71b04a2c41b09ec94c75e710f86d801667689f
aude-la- <aude-la-@student.42madrid.com>
2025-02-14 10:47:20 +0100

    exam rank 05

---
+++ cpp_module01/Warlock.cpp
@@ -0,0 +1,48 @@
+#include "Warlock.hpp"
+
+Warlock::Warlock(const std::string& name, const std::string& title) : name(name), title(title) {
+    std::cout << this->name << ": This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock() {
+    std::cout << this->name << ": My job here is done!" << std::endl;
+
+    for (std::map<std::string, ASpell*>::iterator it = spells.begin(); it != spells.end(); ++it)
+        delete it->second;
+    spells.clear();
+}
+
+const std::string& Warlock::getName() const {
+    return this->name;
+}
+
+const std::string& Warlock::getTitle() const {
+    return this->title;
+}
+
+void Warlock::setTitle(const std::string& newTitle) {
+    this->title = newTitle;
+}
+
+void Warlock::introduce() const {
+    std::cout << this->name << ": I am " << this->name << ", " << this->title << "!" << std::endl;
+}
+
+void Warlock::learnSpell(ASpell* spell) {
+    if (spell)
+        spells[spell->getName()] = spell->clone();
+}
+
+void Warlock::forgetSpell(const std::string& spellName) {
+    std::map<std::string, ASpell*>::iterator it = spells.find(spellName);
+    if (it != spells.end()) {
+        delete it->second;
+        spells.erase(it);
+    }
+}
+
+void Warlock::launchSpell(const std::string& spellName, const ATarget& target) {
+    std::map<std::string, ASpell*>::iterator it = spells.find(spellName);
+    if (it != spells.end())
+        it->second->launch(target);
+}
