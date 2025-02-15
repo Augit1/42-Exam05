492cd61f59331e0557db4e6824661266f5d59d33
aude-la- <aude-la-@student.42madrid.com>
2025-02-15 19:36:01 +0100

    clean code warlock

--- cpp_module00/Warlock.hpp
+++ cpp_module00/Warlock.hpp
@@ -4,24 +4,25 @@
 #include <iostream>
 #include <string>
 
-class Warlock {
-private:
-    std::string name;
-    std::string title;
+class Warlock
+{
+	private:
+		std::string name;
+		std::string title;
 
-    Warlock();
-    Warlock(const Warlock& other);
-    Warlock& operator=(const Warlock& other);
+		Warlock();
+		Warlock(const Warlock& other);
+		Warlock& operator=(const Warlock& other);
 
-public:
-    Warlock(const std::string& name, const std::string& title);
-    ~Warlock();
+	public:
+		Warlock(const std::string& name, const std::string& title);
+		~Warlock();
 
-    const std::string& getName() const;
-    const std::string& getTitle() const;
-    void setTitle(const std::string& newTitle);
+		const std::string& getName() const;
+		const std::string& getTitle() const;
+		void setTitle(const std::string& newTitle);
 
-    void introduce() const;
+		void introduce() const;
 };
 
 #endif
